982a1a0086310633dcfdb52a86629c7384cf4f7a
twbikeman <bikeman@maill2000.com.tw>
2020-04-30 05:48:50 +0800

    Add files via upload

---
+++ test/test_flask_1.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', methods = ['GET','POST'])
+def home():
+    return "success!"
+if __name__ == '__main__':
+    app.run(host = '127.0.0.1', port = 80, debug = True)
