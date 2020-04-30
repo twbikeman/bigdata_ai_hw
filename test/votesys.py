1efd1de82dc8acad9d9efc192a388e25a99e4904
Bryan Tsai <twbikeman@gmail.com>
2020-04-30 11:10:49 +0800

    4/30 11:00

--- test/votesys.py
+++ test/votesys.py
@@ -3,6 +3,15 @@ import requests
 import sys
 import client_ui
 import env_config
+
+
+model = {
+    'country': None,
+    'candiate': None,
+    'votes': None
+}
+
+
 app = QtWidgets.QApplication([])
 ui = client_ui.Ui_MainWindow()
 
@@ -12,17 +21,24 @@ class ConsoleEvent(QtWidgets.QMainWindow):
         self.ui = ui
         self.ui.setupUi(self)
         self.ui.pushButton.clicked.connect(self.send_req)
+        self.ui.comboBox.activated.connect(self.show_Region)
+
     def send_req(self):
         result = requests.get(env_config.url)
+        #model[votes] = self.ui.textEdit.text()
+        print(model)
         print(result.content)
 
-        
+    def show_Region(self):
+        model['country'] = self.ui.comboBox.currentText();
+        print(model)
+
 
 window = ConsoleEvent()
 window.ui.comboBox.clear()
 for country in env_config.countries:
     window.ui.comboBox.addItem(country)
-    
+
 window.ui.comboBox_2.clear()
 for candidate in env_config.candidates:
     window.ui.comboBox_2.addItem(candidate)
