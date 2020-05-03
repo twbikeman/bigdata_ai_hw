8f72468801bc7b82543770efa6b8dc6af5d60380
Bryan Tsai <twbikeman@gmail.com>
2020-05-04 06:00:33 +0800

    fix outlay

--- test/test_flask_1.py
+++ test/test_flask_1.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route('/', methods = ['GET','POST'])
-def home():
+@app.route('/<string:name>', methods = ['GET','POST'])
+def home(name):
+    request.form['test']
     return "success!"
 if __name__ == '__main__':
     app.run(host = '127.0.0.1', port = 80, debug = True)
--- test/test_requests_apigee.py
+++ test/test_requests_apigee.py
@@ -1,8 +1,8 @@
 import requests
-# url = 'https://bikeman-eval-prod.apigee.net/35_tsai'
-# key = 'I79pFngD170XrUw4dnyCEqEso9ltu0oA'
-# result = requests.get(url = url, headers = {'apikey': key})
-# print(result.text)
-
-result = requests.post('http://127.0.0.1/')
+url = 'https://bikeman-eval-prod.apigee.net/35_tsai'
+key = 'I79pFngD170XrUw4dnyCEqEso9ltu0oA'
+result = requests.get(url = url, headers = {'apikey': key})
 print(result.text)
+
+# result = requests.post('http://127.0.0.1/')
+# print(result.text)
