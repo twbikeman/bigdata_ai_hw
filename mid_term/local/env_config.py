5f2855f0fa043de4ade0f1b671be70a9ee8816ef
Bryan Tsai <twbikeman@gmail.com>
2020-05-12 21:29:54 +0800

    fixed apigee problem

--- mid_term/local/env_config.py
+++ mid_term/local/env_config.py
@@ -1,6 +1,7 @@
 countries = ['基隆市','臺北市','新北市','桃園縣','新竹市','新竹縣','苗栗縣','臺中市','彰化縣','南投縣','雲林縣','嘉義市','嘉義縣','臺南市','高雄市','屏東縣','臺東縣','花蓮縣','宜蘭縣','澎湖縣','金門縣','連江縣']
 candidates = ['韓國瑜 張善政', '宋楚瑜 余湘', '蔡英文 賴清德']
-url = 'https://aibigdata-ntut-107598064.an.r.appspot.com'
+# url = 'https://aibigdata-ntut-107598064.an.r.appspot.com'  
+url = 'https://bikeman-eval-prod.apigee.net/35_tsai'
 model = {
     'country': None,
     'candidate': None,
